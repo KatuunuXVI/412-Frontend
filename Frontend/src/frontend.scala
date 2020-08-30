132bff03ce541e853de9a172217821e753765c18
Nathan E <nee2@rice.edu>
2020-08-30 15:48:33 -0500

    Operator Parsing

--- Frontend/src/frontend.scala
+++ Frontend/src/frontend.scala
@@ -1,48 +1,120 @@
+
+
 import scala.io.Source
 import scala.collection.mutable
 import scala.util.control._
 object Frontend {
-  val ILOCfunctions: mutable.Set[String] = mutable.Set("load","loadI","store","add","sub","mult","lshift","rshift","output","nop","//");
+  val ILOCfunctions: mutable.Set[String] = mutable.Set("load","loadI","store","add","sub","mult","lshift","rshift","output","nop");
+  var successfulScan: Boolean = true
+  val operations: List[Operation] = List.empty
   def main(args: Array[String]): Unit = {
-    println(ILOCfunctions)
     for(arg <- args) {
       val filename = arg
-      var lineNumber: Int = 0
-      for (line <- Source.fromFile("inputs/parse.i").getLines) {
-        parseLine(line,lineNumber)
+      //println(arg)
+      var lineNumber: Int = 1
+      val input = Source.fromFile(filename)
+      for (line <- input.getLines.map(trimWhiteSpace)) {
+        if(!line.isEmpty && !isComment(line)) {
+          println("Parsing Line " + lineNumber + ": " + line)
+          parseLine(line,lineNumber)
+        }
         lineNumber += 1
       }
+      input.close()
     }
+
   }
 
-  def parseLine(line: String, lineNumber: Int): Unit = {
-    println(line)
-    def valid: Boolean = true
-    if(line.isEmpty) return
+  def isComment(word:String): Boolean = word.substring(0,2) =="//"
+
+  def isValidOperation(word: String): Boolean = ILOCfunctions.contains(word)
+
+  def isValidRegister(word: String): Boolean = {
+    val commaHead = word(0) == ','
+    val commaTail = word.last == ','
+    val regLabel = word(0) == 'r'
+    val regSecond = word(1) == 'r'
+    val regNumbered = isValidConstant(word.substring(1))
+    val regNumberedCH = isValidConstant(word.substring(2,word.length))
+    val regNumberedCT = isValidConstant(word.substring(1,word.length-1))
+    val regNumberedCHCT  = isValidConstant(word.substring(2, word.length-1))
+    (commaHead && regSecond && regNumberedCHCT && commaTail) || /** ,rx, **/
+      (regLabel && regNumberedCT && commaTail) || /** rx, **/
+      (commaHead && regSecond && regNumberedCH) || /** ,rx **/
+      (regLabel && regNumbered) /** rx **/
+  }
+
+  def isValidConstant(word: String): Boolean = !word.forall(_.isDigit)
+
+  def parseWord(line: String): (String, String, Boolean) = {
+    var potCom: Boolean = false
+    var comment: Boolean = false
     var index: Integer = 0
-    val functionCall: String = trimWhiteSpace(line)
-    if(functionCall.isEmpty) return
-    var potentFuncs = ILOCfunctions
-    //val loopBreak: Breaks = new Breaks;
-    while(potentFuncs.size > 0 && functionCall(index) != ' ') {
-      potentFuncs = potentFuncs.filter(f => index < f.length && f(index) == functionCall(index))
-      println(functionCall(index) + "-> Potential Functions: " + potentFuncs)
-      index += 1
+    val builder: String = trimWhiteSpace(line)
+    var word: String = ""
+    def curChar: Char = builder(index)
+    while(!comment && curChar != ' ' && index < builder.length) {
+      if(curChar == '/') {
+        if(!potCom) {word += curChar; potCom = true} else {comment = true; word = word.substring(0,index-1)}
+        index += 1
+      } else {
+        if(potCom) potCom = false
+        word += curChar
+        index += 1
+      }
     }
-    if(potentFuncs.isEmpty) {
-      println("Line " + lineNumber + ": " + functionCall.substring(0,index) + " is not a valid word")
+    (word, trimWhiteSpace(line.substring(index)), comment)
+  }
+
+  def parseLine(line: String, lineNumber: Int): Option[Operation] = {
+    var parse: String = line
+    var comment: Boolean = false
+    val operationParse = parseWord(line)
+    val operation: String = operationParse._1
+    parse = operationParse._2
+    comment = operationParse._3
+    if(comment) {
+      if(operation.isEmpty) {
+        return Option.empty
+      } else {
+        if(isValidOperation(operation)) System.err.println("Line " + lineNumber + ": Missing Parameters for " + operation) else System.err.println("Line " + lineNumber + ": " + operation + " is not a valid word")
+        return Option.empty
+      }
     }
-    if(potentFuncs.toList(0) == "//") {
-      return
+    if(isValidOperation(operation)) println("Valid Operator") else System.err.println("Line " + lineNumber + ": " + operation + " is not a valid word")
+    Option.empty
+  }
+
+  def parseTwoPar(operation: String, line: String, lineNumber: Integer): Option[String] = {
+    if (operation == "loadI") {
+      var index: Int = 0
+
+      def curChar: Char = line(index)
+
+      var potCom: Boolean = false
+      var comment: Boolean = false
+      var constant: String = ""
     }
+    ???
+  }
+def parseThreePar(operation: String): Unit = {
+
+  }
+
+  def parseOutput(): Unit = {
+
   }
 
   def trimWhiteSpace(line: String): String = {
+    if(line.isEmpty) return line
     var index: Int = 0
-    while(line(index) == ' ') {
+    while(index < line.length && line(index) == ' ') {
       index += 1
     }
-    line.substring(index)
+    if(index < line.length) line.substring(index) else ""
   }
 
+  case class Load(register1: String, register2: String)
+
+  abstract class Operation
 }
